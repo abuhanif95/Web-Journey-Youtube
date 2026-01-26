bedf2d305720647ca95338ab77184dbd1498fcb9
Hanif <mohammadabuhanif95@gmail.com>
2026-01-26 17:54:03 +0800

    JavaScript data types, strict mode, and typeof operator
    - Enable strict mode with "use strict"
    - Use console.log() instead of alert() for Node.js compatibility
    - Declare variables with different data types:
      * string, number, boolean, null, undefined
    - Learn typeof operator to check variable types

---
+++ JS/02 Datatypes and ECMA standards/script.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); //it will work in browser not in terminal because we are using node js
+
+console.log(3 + 3);
+console.log("SHT");
+
+let name = "sft hanif";
+let age = 22;
+let isLoggedIn = false;
+let temp = null;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof temp);
+console.log(typeof state);
