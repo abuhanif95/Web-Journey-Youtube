ad8d242845a9c89a1f180682e3f4fbd280527ccb
Hanif <mohammadabuhanif95@gmail.com>
2026-01-26 22:29:05 +0800

    learn JavaScript type conversion - Use Number(), Boolean(), String() for explicit type conversion - Explore truthy/falsy behavior and edge cases like NaN - Verify conversions with typeof operator

---
+++ JS/03 Datatype Conversion/script.js
@@ -0,0 +1,34 @@
+// let score = "33";
+// let score = "33S";
+// let score = null;
+// let score = true;
+// let score = false;
+// let score = undefined;
+let score = "SHT";
+console.log(typeof score);
+console.log(typeof score);
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+let isLoggedIn = "SHT";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "SHT" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
