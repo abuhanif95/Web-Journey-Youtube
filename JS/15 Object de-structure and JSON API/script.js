c0e7000a87629bbda9ed434b706885e533dd1e81
Hanif <mohammadabuhanif95@gmail.com>
2026-01-31 21:55:35 +0800

    object destructuring and API response structure
    - Object destructuring using const {property: newName} = object syntax
    - Renaming extracted properties during destructuring for cleaner code
    - Practical example with course object and instructor property extraction
    - Commented example of function parameter destructuring pattern
    - API response structure examples showing common JSON formats (object and array of objects)

---
+++ JS/15 Object de-structure and JSON API/script.js
@@ -0,0 +1,29 @@
+const course = {
+  courseName: "JavaScript",
+  price: "1100",
+  courseInstructor: "sft",
+};
+
+// course.courseInstructor
+const { courseInstructor: instructor } = course;
+// console.log(courseInstructor);
+console.log(instructor);
+
+/* 
+ const navbar = ({company}) => {
+ }
+
+navbar(company = "SHT")
+*/
+
+// {
+//     name : 'sft',
+//     courseName : 'Javascript',
+//     price ; 'free',
+// }
+
+[
+    {},
+    {},
+    {}
+]
