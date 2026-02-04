fb31a57491422f3bd50b1918f21b0326dfcc8363
Hanif <mohammadabuhanif95@gmail.com>
2026-02-04 23:56:58 +0800

    practising for loop

---
+++ JS/24 For loop with break and continue/script.js
@@ -0,0 +1,11 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+  const element = index;
+  if (element === 7) {
+    console.log("5 is best number");
+  }
+  console.log(element);
+}
+
+// console.log(element);
