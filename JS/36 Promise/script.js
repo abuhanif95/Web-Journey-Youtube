65a93ddcc35ae4dd1bd2c0476bf79227d406a539
Hanif <mohammadabuhanif95@gmail.com>
2026-03-02 21:11:40 +0800

    JavaScript Promises and async/await
    Practice promise creation, consumption, error handling,
    and fetching API data with both .then() and async/await

---
+++ JS/36 Promise/script.js
@@ -0,0 +1,99 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  // Do an async task
+  // DB calls, cryptography, network
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ name: "sft", email: "sft@gmail.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = false;
+    if (!error) {
+      resolve({ name: "sht", password: "1234" });
+    } else {
+      reject("ERROR! Something went wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.name;
+  })
+  .then((name) => {
+    console.log(name);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("The promise is either resolved or rejected");
+  });
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ name: "sht", password: "1234" });
+    } else {
+      reject("ERROR! Something went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
